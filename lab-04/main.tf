5e3086e5c194ff21c32f41daf4f97bbe97e9b984
virginaj <vajacob@comcast.net>
2018-09-18 16:07:09 -0500

    Today's update

---
+++ lab-04/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+
+  }
+
+data "aws_ami" "lab_04" {
+  most_recent = true
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-6871a115"
+  instance_type = "t2.micro"
+  security_groups = [
+        "launch-wizard-3"
+    ]
+  key_name = "fullstack"
+  tags {
+    Name = "lab_04"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y"
+    ]
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key  = "${file("~/Downloads/fullstack.pem")}"
+  }
+  }
+  
+   provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.web.public_ip}, install-jenkins.yml"
+    }
+  }
