3134a83b4ee54a516d5e8cbc35d11d8f48b5281e
virginaj <42753466+virginaj@users.noreply.github.com>
2018-09-19 08:49:04 -0500

    Update variables.tf

--- lab-05/variables.tf
+++ lab-05/variables.tf
@@ -3,6 +3,7 @@ variable "public_key_path" {
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
+
 Example: ~/.ssh/terraform.pub
 DESCRIPTION
 }
